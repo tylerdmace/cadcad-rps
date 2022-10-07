4ba87f3b3a3977ed4717683997bc83fc57ec4e26
BlockScience Labs <jupyter@blocksciencelabs.com>
2022-10-07 09:59:43 +0000

    Testing imports and docsite structure.

--- docs/index.md
+++ docs/index.md
@@ -3,3 +3,18 @@
 ```python
 --8<-- "model/config.py"
 ```
+
+## psubs.py
+```python
+--8<-- "model/psubs.py"
+```
+
+## state.py
+```python
+--8<-- "model/state.py"
+```
+
+## updates.py
+```python
+--8<-- "model/updates.py"
+```
--- model/config.py
+++ model/config.py
@@ -1,1 +1,18 @@
-exp = "test"
+from cadCAD.configuration.utils import config_sim
+from cadCAD.configuration import Experiment
+from model.state import genesis_states
+from model.psubs import partial_state_update_blocks
+
+simulation_parameters = {
+    'T': range(10),
+    'N': 100,
+}
+
+exp = Experiment()
+c = config_sim(simulation_parameters)
+exp.append_configs(
+    model_id="rps",
+    initial_state=genesis_states,
+    partial_state_update_blocks=partial_state_update_blocks,
+    sim_configs=c
+)
